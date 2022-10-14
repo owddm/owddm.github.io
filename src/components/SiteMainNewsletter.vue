85a31c6725b1f70fa1e664807dabc00b9440efb1
Gregory King <kinxiel@gmail.com>
2022-10-14 23:58:16 +0900

    Added some minor changes, based on the review comments.

--- src/components/SiteMainNewsletter.vue
+++ src/components/SiteMainNewsletter.vue
@@ -1,15 +1,16 @@
 <template>
   <section>
-    <h2>NEWSLETTER</h2>
+    <h2 class="capitalize">newsletter</h2>
     <form class="newsletter">
       <label class="sr-only" for="mailList">Email</label>
-      <input
-        type="email"
-        id="mailList"
-        name="mailList"
-        placeholder="email address"
-      />
+      <input type="email" id="mailList" name="mailList" placeholder="email address" />
       <button type="submit">SUBSCRIBE</button>
     </form>
   </section>
 </template>
+
+<style scoped>
+.capitalize {
+  text-transform: uppercase;
+}
+</style>
