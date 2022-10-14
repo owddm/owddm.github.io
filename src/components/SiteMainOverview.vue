8f0d9482c260a539dbbd8cb033715d49de938360
Gregory King <kinxiel@gmail.com>
2022-10-15 00:10:49 +0900

    Updated content for What We Do.

--- src/components/SiteMainOverview.vue
+++ src/components/SiteMainOverview.vue
@@ -1,28 +1,30 @@
 <template>
   <section>
-    <h2>WHAT WE DO</h2>
-    <ul class="list-reset info-list">
-      <li>
-        <span class="info-list-title">PRACTICE</span>
-        <p>
-          No practice, no improvements.<br />Here is for practicing your
-          presentation skills.
-        </p>
-      </li>
-      <li>
-        <span class="info-list-title">LEARN</span>
-        <p>
-          No practice, no improvements.<br />Here is for practicing your
-          presentation skills.
-        </p>
-      </li>
-      <li>
-        <span class="info-list-title">SHARE YOUR INTEREST</span>
-        <p>
-          No practice, no improvements.<br />Here is for practicing your
-          presentation skills.
-        </p>
-      </li>
-    </ul>
+    <h2 class="capitalize">What we do</h2>
+    <div>
+      <p>We are an independent group of designers and developers in the Kansai (Osaka/Kyoto) area of Japan.</p>
+      <p>To practice our English presentations skills, learn new things and meet other people we are organizing IRL (in-person) events.</p>
+      <p>All our events are organized by volunteers. Any money that we collect is just to cover our costs.</p>
+    </div>
   </section>
 </template>
+
+<style scoped>
+div {
+  padding: 0.5rem;
+}
+
+p {
+  margin-top: 1rem;
+  font-size: 1.2rem;
+  letter-spacing: 0.025em;
+}
+
+p:first-of-type {
+  text-indent: 1rem;
+}
+
+.capitalize {
+  text-transform: uppercase;
+}
+</style>
