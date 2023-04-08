191b78390c6327641686e2b310edbc0948d70cde
Martin Heidegger <martin.heidegger@gmail.com>
2023-04-09 02:39:54 +0900

    Removed old layout settings

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,8 @@
   <div class="container">
     <SiteHeader />
     <main><slot /></main>
-    <div class="footer">
-      <SiteFooter />
-    </div>
+    <SiteFooter />
   </div>
 </template>
-
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-  padding: 3rem;
-}
-.footer {
-  margin-top: 3rem;
-}
 </style>
