3442fcebd647a39579c5475e96195d959add60d8
Martin Heidegger <martin.heidegger@gmail.com>
2023-04-11 00:33:28 +0900

    refactor:: use computed instead of watchEffect

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,16 @@
 <template>
-  <Map class="main-map" :key="update" :markers="markers" />
+  <Map class="main-map" :key="update" :markers="markers" v-if="markers"/>
 </template>
 <script setup type="ts">
 import Map from '~~/components/Map.vue';
 import { useEvents, getLatestEvents } from '~~/utils/events';
 
 const { data: events } = await useEvents()
-const markers = ref([])
 const update = ref(Date.now())
-
-watchEffect(() => {
-  if (!events.value) return
-  const latest = getLatestEvents(events.value)
+const markers = computed(() => {
   update.value = Date.now()
-  markers.value = latest.map(event => {
+  if (!events.value) return null
+  return getLatestEvents(events.value).map(event => {
     return {
       lat: event.venue.lat,
       lng: event.venue.lng,
--- pages/photos.vue
+++ pages/photos.vue
@@ -4,12 +4,11 @@
 
 <script setup lang="ts">
 import { usePhotos, Group } from "~~/utils/photos";
-const groups = ref<Group[]>([]);
-const update = ref(Date.now());
 const { data } = await usePhotos();
-watchEffect(() => {
-  if (!data.value) return;
-  groups.value = Array.from(data.value!).reverse();
+const update = ref(Date.now());
+const groups = computed<Group[]>(() => {
   update.value = Date.now();
+  if (!data.value) return [];
+  return Array.from(data.value!).reverse();
 });
 </script>
