9c08570bb24c6fe23190f0415cc82ed2ae73bf3c
Martin Heidegger <martin.heidegger@gmail.com>
2023-06-20 01:54:26 +0900

    fixing suggestions by coliff.

--- pages/photos.vue
+++ pages/photos.vue
@@ -6,7 +6,7 @@
         <li>All photos here have been taken by community members and sent in either through our <a href="https://discord.com/channels/1034792577293094972/1077517983439654962">#event-photos</a>
       channel on discord or through the Meetup page.</li>
         <li>All photos in this channel are published under the <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons BY-NC-SA license</a>.</li>
-        <li>If you want a photo of you removed from the page, or if yo have any other issues with a photo on this page, please let us know through → <a href="https://github.com/owddm/public/issues/new">a new github issue</a>.</li>
+        <li>If you want a photo of you removed from the page, or if you have any other issues with a photo on this page, please let us know through → <a href="https://github.com/owddm/public/issues/new">a new GitHub issue</a>.</li>
         <li>If you use any photo of this page, we request you to follow the github repository and also remove any photo if a member is uncomfortable with it.</li>
       </ul>
     </details>
