f14c40b71953d86eea052c881b490ad1bee5f2af
Martin Heidegger <martin.heidegger@gmail.com>
2024-10-03 01:40:29 +0900

    fix: making sure that map on events page is shown

--- src/components/EventMap.tsx
+++ src/components/EventMap.tsx
@@ -7,11 +7,29 @@ export type EventMapProps = {
   className?: string;
   markers: MapMarker[];
 };
+// const key = "8JZtFquVr4EgrXWK57Kx";
+// const mapId = "53ec7d02-deae-4eb4-8294-406ed07b3271";
+
+// export const EventMap = ({ markers, className }: EventMapProps) => {
+//   const mapStyle = `https://api.maptiler.com/maps/${mapId}/style.json?key=${key}`
+//   if (!globalThis.document) return <></>
+//   console.log(mapStyle)
+//   return <Map
+//     initialViewState={{
+//       longitude: -122.4,
+//       latitude: 37.8,
+//       zoom: 14
+//     }}
+//     style={{width: 600, height: 400}}
+//     mapStyle={mapStyle}>
+//   </Map>
+// }
 export const EventMap = ({ markers, className }: EventMapProps) => {
   const map = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    if (!map.current) return;
-    if (!globalThis) return;
+    const mapNode = map.current;
+    if (!mapNode) return;
+    if (!globalThis.document) return;
     let mounted = true;
     let lMap: any;
     let stop = () => {};
@@ -23,16 +41,25 @@ export const EventMap = ({ markers, className }: EventMapProps) => {
       if (!mounted) {
         return;
       }
-      lMap = L.map(map.current!).setView([34.95937, 136.1], 9.4);
+      const marker = markers[0];
+      if (!marker) {
+        return;
+      }
+      console.log({ marker });
+      if (marker.type == "owddm") {
+        lMap = L.map(mapNode).setView([34.69937, 135.49081], 11);
+      } else if (markers![0]!.type == "kwddm") {
+        lMap = L.map(mapNode).setView([35.00005, 135.75581], 11);
+      }
       const gl = new MaplibreLayer({
         attribution: '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
         style: `https://api.maptiler.com/maps/${mapId}/style.json?key=${key}`,
       });
       gl.addTo(lMap);
       stop = (() => {
-        const lMarkers = markers.map((marker) => {
+        const lMarkers = markers!.map((marker) => {
           let title = marker.title
-            ? `<div class="map-event--label"><div class="map-event--title">${marker.title}</div>${marker.subtitle ? `<div class="map-event--info">${marker.subtitlePrefix ? `<strong>${marker.subtitlePrefix}</strong> ` : ""}${marker.subtitle}</div></div>` : ""}`
+            ? `<div class="map-single-event--label"><div class="map-single-event--title">${marker.title}</div>${marker.subtitle ? `<div class="map-single-event--info">${marker.subtitlePrefix ? `<strong>${marker.subtitlePrefix}</strong> ` : ""}${marker.subtitle}</div></div>` : ""}`
             : "";
           const html = `<img src="/images/marker/${marker.type}.svg" class="map-event--image" alt="">${title}`;
           return L.marker([marker.lat, marker.lng], {
@@ -43,9 +70,7 @@ export const EventMap = ({ markers, className }: EventMapProps) => {
               html,
             }),
           }).on("click", () => {
-            if (marker.target) {
-              document.location.href = marker.target;
-            }
+            console.log({ click: marker });
           });
         });
         for (const marker of lMarkers) {
@@ -69,7 +94,7 @@ export const EventMap = ({ markers, className }: EventMapProps) => {
   }, [map.current]);
   return (
     <div className={clsx("map--container", className)}>
-      <div className="map--map" ref="map">
+      <div className="map--map" ref={map}>
         <a href="https://www.maptiler.com" style={{ position: "absolute", left: "10px", bottom: "10px", zIndex: 999 }}>
           <img src="https://api.maptiler.com/resources/logo.svg" alt="MapTiler logo" />
         </a>
