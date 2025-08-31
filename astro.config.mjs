e4d264ffa2def21d35681aef57000a97f7bdf7ba
Martin Heidegger <martin.heidegger@gmail.com>
2025-08-31 15:34:40 +0900

    fix: redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import AstroReact from "@astrojs/react";
-import * as redirects from "./redirects.json";
+import redirects from "./redirects.json";
 
 // https://astro.build/config
 export default defineConfig({
