0498a7ab2e9d1133f8327a53ec36fb0391338b33
Martin Heidegger <martin.heidegger@gmail.com>
2025-08-31 15:29:19 +0900

    fix: redirecting to oktech.jp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import AstroReact from "@astrojs/react";
+import * as redirects from './redirects.json';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [AstroReact({})],
-  redirects: {
-    discord: "https://discord.com/invite/k8xj8d75f6",
-  },
+  redirects,
 });
